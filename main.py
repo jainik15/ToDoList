ab91800695f649e6ca8b8f166bbde6539d798be4
jainik15 <jxinik15@gmail.com>
2025-07-16 21:33:52 +0530

    Bug fixes

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-import sys 
 def addTask():
     t= input("Enter task desciption: ")
     todo_list.append(t)
     print("Task added successfully")
-    main()
+
 
 def removeTask():
     if len(todo_list)==0:
         print("List is empty")
         
     else:
-        i=1
-        for tasks in todo_list:
-            print(i, tasks)
-            i+=1
+        view_list()
         try:
-            removeTask = int(input("Enter number to remove: "))
-            todo_list.remove(todo_list[removeTask])
-            print("Task removed successfuly")
-        except:
-            print("Invalid arguements")
-        main()
+            task_num = int(input("Enter task number: "))
+            idx= task_num-1
+            if 0<=idx<len(todo_list):
+                removed_task = todo_list.pop(idx)
+                print(removed_task, " task removed successfully")
+            else:
+                print("Invalid task number")
+        except ValueError:
+            print("Invalid arguments")
 
 def view_list():
     if len(todo_list)==0:
@@ -33,38 +32,35 @@ def view_list():
             i+=1
         print()
         print("END OF LIST")
-    main()
-
 
 todo_list = [] #creating list
 
 #Displaying actions menu
 def main():
-    print("--TO DO LIST--")
-    print("1. Add")
-    print("2. Remove")
-    print("3. View")
-    print("4. Exit")
-
     while True:
+        print("--TO DO LIST--")
+        print("1. Add")
+        print("2. Remove")
+        print("3. View")
+        print("4. Exit")
+
         try:
-            user_choice = int(input("Enter your choice: ")) 
-            if 0< user_choice <5:
-                choice=user_choice
+            choice = int(input("Enter your choice: ")) 
+
+            if choice==1:
+                addTask()
+            elif choice==2:
+                removeTask()
+            elif choice==3:
+                view_list()
+            elif choice==4:
+                print("--Program Ended--")
                 break
             else:
-                print("Invalid choice! Enter between range 1-4")
+                print("Invalid choice! (Enter between range 1-4)")
+       
         except ValueError:
             print("Invalid choice!")
-
-    if choice==1:
-        addTask()
-    elif choice==2:
-        removeTask()
-    elif choice==3:
-        view_list()
-    elif choice==4:
-        sys.exit()
 
 if __name__=="__main__":
     main()
