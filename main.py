49fa8706c1db5815c7311530799d9febea9d7c5e
jainik15 <jxinik15@gmail.com>
2025-07-16 23:34:54 +0530

    added due dates, marks tasks functionalities

--- main.py
+++ main.py
@@ -1,49 +1,109 @@
-def addTask():
-    t= input("Enter task desciption: ")
-    todo_list.append(t)
-    print("Task added successfully")
+import json
+from datetime import datetime
 
+todo_list = [] 
+
+def saveTasks():
+    with open("tasks.json", "w") as file:
+        json.dump(todo_list, file)
+
+def loadTasks():
+    global todo_list
+    try:
+        with open("tasks.json", "r") as file:
+            todo_list= json.load(file)
+    except FileNotFoundError:
+        todo_list=[]
+
+def addTask():
+    print()
+    tname= input("Enter task desciption: ")
+    due_date= input("Enter due date: [yyyy-mm-dd]").strip()
+    if due_date=="":
+        due_date=None
+    else:
+        try:
+            datetime.strptime(due_date, "%Y-%m-%d")
+        except ValueError:
+            print("Invalid date, saving due date as none")
+            due_date=None
+    task = {"description" : tname , "completed" : False, "due" : due_date}
+    todo_list.append(task)
+    print()
+    print("Task added successfully")
+    saveTasks()
 
 def removeTask():
     if len(todo_list)==0:
         print("List is empty")
-        
     else:
         view_list()
         try:
-            task_num = int(input("Enter task number: "))
+            print()
+            task_num = int(input("Enter task number to remove: "))
             idx= task_num-1
             if 0<=idx<len(todo_list):
                 removed_task = todo_list.pop(idx)
-                print(removed_task, " task removed successfully")
+                print()
+                print("'",removed_task["description"],"'", " task removed successfully")
+                saveTasks()
             else:
                 print("Invalid task number")
         except ValueError:
             print("Invalid arguments")
+
+def markTasks():
+    if(len(todo_list) ==0 ):
+        print("List is empty")
+    else:
+        view_list()
+        try:
+            print()
+            task_num=int(input("Enter task number to mark: "))
+            idx=task_num-1
+            if 0<=idx<len(todo_list):
+                todo_list[idx]["completed"]=True
+                saveTasks()
+                print("'",todo_list[idx]["description"],"'", "marked completed successfully")
+            else:
+                print("Invalid task number")
+        except ValueError:
+            print("Invalid arguments")
+
 
 def view_list():
     if len(todo_list)==0:
         print("List is empty")
     else:
         i=1
-        print("-To Do List-")
-        for tasks in todo_list:
-            print(" ",i, tasks)
+        print("-----To Do List-----")
+        def sort_key(task):
+            if task["due"] is None:
+                return datetime.max
+            try: 
+                return datetime.strptime(task["due"], "%Y-%m-%d")
+            except ValueError:
+                return datetime.max
+            
+        sorted_tasks= sorted(todo_list,key=sort_key)
+
+        for task in sorted_tasks:
+            status = "[x]" if task["completed"]==True else "[ ]"
+            due_date = f"(Due: {task["due"]})" if {task["due"]} != None else ""
+            print (f'{status} {i}. || {task["description"]} || {due_date}')
             i+=1
-        print()
-        print("END OF LIST")
+        print("-----End of List-----")
 
-todo_list = [] #creating list
-
-#Displaying actions menu
 def main():
     while True:
-        print("--TO DO LIST--")
-        print("1. Add")
-        print("2. Remove")
-        print("3. View")
-        print("4. Exit")
-
+        print()
+        print("-----TO DO LIST-----")
+        print("1. Add Task")
+        print("2. Remove Task")
+        print("3. Mark Task")
+        print("4. View Task")
+        print("5. Exit ")
+        print()
         try:
             choice = int(input("Enter your choice: ")) 
 
@@ -52,15 +112,18 @@ def main():
             elif choice==2:
                 removeTask()
             elif choice==3:
-                view_list()
+                markTasks()
             elif choice==4:
-                print("--Program Ended--")
+                view_list()
+            elif choice==5:
+                print("--Program Terminated--")
                 break
             else:
-                print("Invalid choice! (Enter between range 1-4)")
+                print("Invalid choice! (Enter between range 1-5)")
        
         except ValueError:
             print("Invalid choice!")
 
 if __name__=="__main__":
+    loadTasks()
     main()
